0079ac726450bef9d812f3e325acad16ee651838
Nicocovi <nico0205@hotmail.com>
2018-08-10 10:39:41 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -23,7 +23,7 @@ node {
 
         stage('Deploy') {
             def branch = ['master']
-            def name = "Spring MS 1"
+            def name = "SpringMS1"
             def path = "build/libs/gs-spring-boot-0.1.0.jar"
             def manifest = "manifest.yml"
             
