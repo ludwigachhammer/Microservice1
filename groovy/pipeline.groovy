edff120ef6a3df950cdeedd20579d2181221b83c
Nicocovi <nico0205@hotmail.com>
2018-08-12 18:03:08 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -38,7 +38,7 @@ node {
                              ]]) {
                 sh 'cf login -a https://api.run.pivotal.io -u $CF_USERNAME -p $CF_PASSWORD --skip-ssl-validation'
                 sh 'cf target -o ga72hib-org -s masterarbeit'
-                sh 'cf push sping-ms1 -f '+manifest+' --hostname '+name+' -p '+path
+                sh 'cf push sping-microservice1 -f '+manifest+' --hostname '+name+' -p '+path
             }
         }
         
