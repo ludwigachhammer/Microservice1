c6d3db3de2c94b458e579ec2aa8c0521b18753e5
Nicocovi <nico0205@hotmail.com>
2018-09-05 16:05:23 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -47,7 +47,7 @@ node {
         }
         
         stage("Push Documentation"){
-            def cmd = "curl -H \"Content-Type: application/json\" -X POST http://192.168.99.100:9123/document -d \"{\"id\": \"34564565\",\"name\": \"spring-microservice-demo\",\"type\": \"service\",\"owner\": \"Nicolas\",\"description\": \"Simple microservice\",\"domain\": \"Finance\"}\" "
+            def cmd = "curl -H \"Content-Type: application/json\" -X POST http://192.168.99.100:9123/document -d {\"id\": \"34564565\",\"name\": \"spring-microservice-demo\",\"type\": \"service\",\"owner\": \"Nicolas\",\"description\": \"Simple microservice\",\"domain\": \"Finance\"} "
             sh cmd//sh
         }//stage
         
