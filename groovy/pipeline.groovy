1fee4d4f22ee35c92abcdd5626f7d627e66d23f6
Nicocovi <nico0205@hotmail.com>
2018-09-06 18:30:43 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -62,7 +62,7 @@ node {
         }
         */
         stage("Push Documentation"){
-            callPost("http://192.168.99.100:9123/document", "{\"id\": \"16486132165561\", \"name\": \"ServiceNow3\", \"owner\": \"Nico\", \"description\": \"bla\", \"short_name\": \"serviceAZ12345\", \"type\": \"service\"}") //Include protocol
+            callPost("http://192.168.99.100:9123/document", "{\"id\": \"16486132165561123\", \"name\": \"ServiceNow4\", \"owner\": \"Nico\", \"description\": \"bla\", \"short_name\": \"serviceAZ12345\", \"type\": \"service\"}") //Include protocol
         }//stage
         
     }
