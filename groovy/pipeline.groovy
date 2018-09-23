ba8b2e8b155c449c42220c86a0f9423cce5001ee
Nicocovi <nico0205@hotmail.com>
2018-09-23 23:43:03 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -104,9 +104,11 @@ node {
             
             def basicinfo = "\"id\": \"098765135465\", \"name\": \"Kick-off-2\", \"owner\": \"Nico\", \"description\": \"bla\", \"short_name\": \"serviceAZ12\", \"type\": \"service\","
             def runtime = " \"runtime\": {\"ram\": \"${APP_SHORTSTATUS[4]}\", \"cpu\": \"${APP_SHORTSTATUS[3]}\", \"disk\": \"${APP_SHORTSTATUS[5]}\", \"host_type\": \"cloudfoundry\" }"
+            def jsonstring = "{"+basicinfo+""+runtime+"}"
+            echo "JSONSTRING: ${jsonstring}"
             
             try {
-                    callPost("http://192.168.99.100:9123/document", "{"+basicinfo+""+runtime+"}") //Include protocol
+                    callPost("http://192.168.99.100:9123/document", jsonstring) //Include protocol
                 } catch(e) {
                     // if no try and catch: jenkins prints an error "no content-type" but post request succeeds
                 }
