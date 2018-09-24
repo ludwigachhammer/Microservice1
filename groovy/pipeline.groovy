6fffe97d7edba373a8e50f7d5a93b37565c1bf49
Nicocovi <nico0205@hotmail.com>
2018-09-24 11:30:46 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -101,7 +101,7 @@ node {
         
         stage("Push Documentation"){
             
-            def basicinfo = "\"id\": \"098765135465\", \"name\": \"Kick-off-2\", \"owner\": \"Nico\", \"description\": \"bla\", \"short_name\": \"serviceAZ12\", \"type\": \"service\","
+            def basicinfo = "\"id\": \"09876513541465\", \"name\": \"Kick-off-3\", \"owner\": \"Nico\", \"description\": \"bla\", \"short_name\": \"serviceAZ12\", \"type\": \"service\","
             def runtime = " \"runtime\": {\"ram\": \"${APP_SHORTSTATUS[4]}\", \"cpu\": \"${APP_SHORTSTATUS[3]}\", \"disk\": \"${APP_SHORTSTATUS[5]}\", \"host_type\": \"cloudfoundry\" }"
             def jsonstring = "{"+basicinfo+""+runtime+""+"}"
             echo "JSONSTRING: ${jsonstring}"
