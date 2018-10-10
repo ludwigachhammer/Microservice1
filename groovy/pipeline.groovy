019df8bf62b6c25985d00992339c70873536e5c6
Nicocovi <nico0205@hotmail.com>
2018-10-10 10:52:04 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -53,7 +53,8 @@ node {
             
             //TODO
             //Validate jira link in links.config
-            String fileContents = new File('links.config').getText('UTF-8')
+            //String fileContents = new File('links.config').getText('UTF-8')
+            File f = new File(this.getClass().getResource("/links.conf").getFile());
             //println  "Root project:   ${project.rootProject}";
             //println  "  rootDir:      ${project.rootDir}"
             //println  "  projectDir:   ${project.projectDir}";
