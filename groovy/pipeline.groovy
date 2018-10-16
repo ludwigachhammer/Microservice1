7b79082e9429909543783e9a147348e372f5b183
Nicocovi <nico0205@hotmail.com>
2018-10-16 10:31:40 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -20,8 +20,8 @@ def callGetJIRA(String urlString) {
     def connection = url.openConnection()
     connection.setRequestMethod("GET")
     def enconded = ("ncorpan:Gringoloco1").bytes.encodeBase64().toString()
-    def basic-auth = "Basic ${encoded}"
-    connection.setRequestProperty("Authorization", basic-auth)
+    def basicauth = "Basic ${encoded}"
+    connection.setRequestProperty("Authorization", basicauth)
     connection.connect()
 
     new groovy.json.JsonSlurper().parseText(connection.content.text)
