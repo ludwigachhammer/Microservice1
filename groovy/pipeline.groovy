1a6b2ca740c7133f9e6dabe3cf0f0e1ae0f15407
Nicocovi <nico0205@hotmail.com>
2018-11-23 11:20:58 +0100

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -91,8 +91,8 @@ node {
         }
         
         stage("Get Basic Jira Information"){
-            //GET http://localhost:8099/rest/api/2/project/{projectIdOrKey}
-            def jiraProject = callGetJira("http://localhost:8099/rest/api/2/project/MAS")
+            //GET http://jira-url:port/rest/api/2/project/{projectIdOrKey}
+            def jiraProject = callGetJira("http://vmmatthes32.informatik.tu-muenchen.de:6000/rest/api/2/project/ED")
             BASIC_INFO = "\"id\": \""+jiraProject.id+"\", \"key\":\""+jiraProject.key+"\", \"name\": \""+jiraProject.name+"\", \"owner\": \""+jiraProject.lead.name+"\", \"description\": \""+jiraProject.description+"\", \"short_name\": \""+jiraProject.key+"\", \"type\": \""+jiraProject.projectTypeKey+"\","
             echo "BASIC INFO: ${BASIC_INFO}"
         }
@@ -100,7 +100,7 @@ node {
             // customfield_10007: Domain
             // customfield_10008: Subdomain
             // customfield_10009: Product
-            def response = callGetJira("http://localhost:8099/rest/api/2/search?jql=project=MAS")
+            def response = callGetJira("http://vmmatthes32.informatik.tu-muenchen.de:6000/rest/api/2/search?jql=project=ED")
             //echo "ISSUES: ${response}"
             List<String> domains = new ArrayList<String>()
             List<String> subdomains = new ArrayList<String>()
@@ -197,7 +197,7 @@ node {
             def jsonstring = "{"+BASIC_INFO+BUSINESS_INFO+","+runtime+","+LINKS+","+APP_SERVICES+"}"
             echo "JSONSTRING: ${jsonstring}"
             try {
-                    callPost("http://192.168.99.100:9123/document", jsonstring) //Include protocol
+                    callPost("http://131.159.30.173:9123/document", jsonstring) //Include protocol
                 } catch(e) {
                     // if no try and catch: jenkins prints an error "no content-type" but post request succeeds
                 }
